d766db592b01435d863e8a0b53610498715dcda3
Alex B <96922302+salutationss@users.noreply.github.com>
2022-06-29 14:11:12 -0400

    commit tree 9f129f481d867c80f02c166e2ebe3203eff6da6b

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -20,7 +20,7 @@ test("renders the h1 with the text 'Name is a Web Developer from City'", () => {
   expect(h1.tagName).toBe("H1"); // check that the element is a <h1>
 });
 
-test("the h1 has a an inline style attribute with a color of 'firebrick'", () => {
+test("the h1 has an inline style attribute with a color of 'firebrick'", () => {
   render(<Home />);
 
   const h1 = screen.queryByText(`${name} is a Web Developer from ${city}`);
